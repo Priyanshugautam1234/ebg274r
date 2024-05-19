0a05a4160b74fabdc1b284651c17a0b0c26b6f5b
kasak yadav <ykasak804@gmail.com>
2024-05-19 17:42:38 +0530

    In this commit I have added background color to status bar fully

--- app/src/main/java/com/org/myapplication/MainActivity.java
+++ app/src/main/java/com/org/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.view.View;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
 
-
+        }
     }
 }
